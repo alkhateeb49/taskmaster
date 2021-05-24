71735e10067c8ca8c46b57ef0eda165d555fb73e
alkhateeb49 <mohamadalkhateeb49@yahoo.com>
2021-05-24 16:24:57 +0300

    lab32

--- TaskMaster/app/src/main/java/com/example/taskmaster/AddTask.java
+++ TaskMaster/app/src/main/java/com/example/taskmaster/AddTask.java
@@ -7,10 +7,16 @@ import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.Task;
 
 import java.util.List;
 
@@ -26,6 +32,17 @@ public class AddTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
         this.setTitle("Add Task");
+
+        try {
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.configure(getApplicationContext());
+
+            Log.i("Tutorial", "Initialized Amplify");
+        } catch (AmplifyException e) {
+            Log.e("Tutorial", "Could not initialize Amplify", e);
+        }
+
+
         title=findViewById(R.id.title);
         body=findViewById(R.id.body);
         add=findViewById(R.id.addTask);
@@ -49,7 +66,20 @@ public class AddTask extends AppCompatActivity {
                         tasks.setTitle(string_Title);
                         tasks.setBody(string_Body);
                         tasks.setState(string_State);
-                        AppDatabase.getDatabase(getApplicationContext()).taskDao().insertAll(tasks);
+//                        AppDatabase.getDatabase(getApplicationContext()).taskDao().insertAll(tasks);
+
+
+                        Task task = com.amplifyframework.datastore.generated.model.Task.builder()
+                                .title(title.getText().toString())
+                                .body(body.getText().toString())
+                                .state(state.getText().toString())
+                                .build();
+
+                        Amplify.DataStore.save(task,
+                                success -> Log.i("Tutorial", "Saved item: " + success.item().getTitle()),
+                                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+                        );
+
                         Intent i=new Intent(AddTask.this,MainActivity.class);
                         startActivity(i);
                     }
@@ -60,6 +90,7 @@ public class AddTask extends AppCompatActivity {
 
             }
         });
+
 
 
     }
